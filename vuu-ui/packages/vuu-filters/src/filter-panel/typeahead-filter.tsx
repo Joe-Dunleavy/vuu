831c4812e50e248b95c4c9405d5661c25e59cbaf
Joe Dunleavy <jdunleavy@scottlogic.com>
2023-01-25 16:48:35 +0000

    plug existing typeahead filter into search bar selector

--- vuu-ui/packages/vuu-filters/src/filter-panel/typeahead-filter.tsx
+++ vuu-ui/packages/vuu-filters/src/filter-panel/typeahead-filter.tsx
@@ -1,23 +1,41 @@
+import { useTypeaheadSuggestions } from "@finos/vuu-data";
 import {
   HTMLAttributes,
   MouseEvent,
   ReactElement,
   SyntheticEvent,
   useCallback,
+  useEffect,
   useState,
 } from "react";
 
 export const typeaheadFilter = (data: string[]) => {
   const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState<
+    string | undefined
+  >(undefined);
 
-  const filterBy = useCallback((e: SyntheticEvent) => {
-    const inputValue = (e.target as HTMLInputElement).value;
+  const getSuggestions = useTypeaheadSuggestions();
 
-    if (inputValue.length > 0) {
-      const regex = new RegExp(`^${inputValue}`, `i`);
-      setSuggestions(data.sort().filter((v) => regex.test(v)));
-    }
-  }, []);
+  useEffect(() => {
+    getSuggestions([
+      { module: "SIMUL", table: "instruments" },
+      "currency",
+    ]).then((response) => setSuggestions(response));
+  }, [getSuggestions]);
 
-  return <input onChange={filterBy} placeholder="filter by..." />;
+  //   const renderSuggestions = useCallback(() => {
+  //     if (suggestions.length === 0) return null;
+  //     return (
+  //       <ul>
+  //         {suggestions.map((city) => (
+  //           <li key={city} onClick={(e) => this.suggestionSelected(city)}>
+  //             {city}
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     );
+  //   }, [suggestions]);
+
+  return <input onChange={getSuggestions} placeholder="filter by..." />;
 };
